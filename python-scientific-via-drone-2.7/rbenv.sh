2f6530dae9c32f0a464b62e5ed951029564de1e0
lpm11 <lpm11r@gmail.com>
2014-05-27 00:51:18 +0900

    Patched for rbenv error.

---
+++ python-scientific-via-drone-2.7/rbenv.sh
@@ -0,0 +1,10 @@
+export PATH="/home/ubuntu/.rbenv/bin:$PATH"
+
+# set default env vars
+export RBENV_ROOT="/home/ubuntu/.rbenv"
+export RBENV_VERSION=2.0.0-p353
+export RAILS_ENV=test
+
+# initialize and set ruby version
+eval "$(rbenv init -)"
+rbenv global 2.0.0-p353
